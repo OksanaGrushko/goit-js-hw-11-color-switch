5beb4f246bdc7a7781f2076505cb0ed5d2329409
OksanaGrushko <okoksana96@gmail.com>
2020-07-30 21:08:19 +0300

    interval color

---
+++ src/file.js
@@ -0,0 +1,43 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const refs = {
+  body: document.querySelector("body"),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
+
+console.log(refs.startBtn);
+
+const randomInterval = {
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      refs.body.style.background =
+        colors[Math.floor(Math.random() * colors.length)];
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener(
+  "click",
+  randomInterval.start.bind(randomInterval)
+);
+refs.stopBtn.addEventListener(
+  "click",
+  randomInterval.stop.bind(randomInterval)
+);
